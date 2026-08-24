38648f4c4d103006af6db60bd1901a08c6956791
fanilran <fanilran@student.42antananarivo.mg>
2026-08-24 16:35:17 +0300

    fix: Modif

--- codexion.h
+++ codexion.h
@@ -6,7 +6,7 @@
 /*   By: fanilran <fanilran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/15 21:05:03 by fanilran          #+#    #+#             */
-/*   Updated: 2026/08/19 17:30:32 by fanilran         ###   ########.fr       */
+/*   Updated: 2026/08/24 14:59:59 by fanilran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,28 @@ typedef struct s_data
 	pthread_mutex_t	print_mutex;
 }	t_data;
 
+typedef struct s_dongle
+{
+	pthread_mutex_t	lock;
+	pthread_cond_t	cond;
+	int				available;
+	long			released_at;
+}	t_dongle;
+
+
 typedef struct s_coder
 {
 	int			id;
 	pthread_t	thread;
+	t_dongle	*left;
+	t_dongle	*right;
+	t_data		*config;
 }	t_coder;
 
-int	pars(t_data *config, int agrc, char *argv[]);
-int	main(int argc, char *argv[]);
+int		pars(t_data *config, int agrc, char *argv[]);
+int		main(int argc, char *argv[]);
+void	*create_threads(t_data config, t_coder *coders);
+void	*routine(void *arg);
+
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fanilran <fanilran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/16 04:30:25 by fanilran          #+#    #+#             */
-/*   Updated: 2026/08/21 15:07:06 by fanilran         ###   ########.fr       */
+/*   Updated: 2026/08/24 16:33:53 by fanilran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,32 @@
 
 void	*routine(void *arg)
 {
-	t_data	*config;
+	t_coder	*coder;
 	int		i;
 
-	config = (t_data *)arg;
+	coder = (t_coder *)arg;
 	i = 0;
-	while (i < config->number_of_compiles_required)
+	while (i < coder->config->number_of_compiles_required)
 	{
 		printf("Compile\n");
-		usleep(config->time_to_compile * 1000);
+		usleep(coder->config->time_to_compile * 1000);
 		i++;
 	}
 	return (NULL);
 }
 
-void	*create_threads(t_data config)
+void	*create_threads(t_data config, t_coder *coders)
 {
 	pthread_t *thread;
-
 	int	i;
-	i = 0;
+
 	thread = malloc(sizeof(pthread_t) * config.number_of_coder);
+	if (!thread)
+		return (NULL);
+	i = 0;
 	while (config.number_of_coder > i)
 	{
-		if (pthread_create(&thread[i], NULL, routine, (void *)&config) != 0)
+		if (pthread_create(&thread[i], NULL, routine, &coders[i]) != 0)
 			return (NULL);
 		i++;
 	}
@@ -47,14 +49,15 @@ void	*create_threads(t_data config)
 		pthread_join(thread[i], NULL);
 		i++;
 	}
-	
+
 	return (NULL);
 }
 
 int	main(int argc, char *argv[])
 {
 	t_data		config;
-	t_coder		*tab;
+	t_coder		*coders;
+	t_dongle	*dongles;
 	int			i;
 
 	if (!pars(&config, argc, (char **)argv))
@@ -62,17 +65,42 @@ int	main(int argc, char *argv[])
 		printf("Error: Regarding the argument!");
 		return (1);
 	}
-	tab = NULL;
-	i = 0;
-	tab = malloc(sizeof(t_coder) * config.number_of_coder);
-	if (!tab)
+	dongles = malloc(sizeof(t_dongle) * config.number_of_coder);
+	if (!dongles)
 		return (1);
+	i = 0;
 	while (i < config.number_of_coder)
 	{
-		tab[i].id = i + 1;
+		pthread_mutex_init(&dongles[i].lock, NULL);
+		pthread_cond_init(&dongles[i].cond, NULL);
+		dongles[i].available = 1;
+		dongles[i].released_at = 0;
 		i++;
 	}
-	create_threads(config);
-	free(tab);
+	coders = malloc(sizeof(t_coder) * config.number_of_coder);
+	if (!coders)
+	{
+		free(dongles);
+		return (1);
+	}
+	i = 0;
+	while (i < config.number_of_coder)
+	{
+		coders[i].id = i + 1;
+		coders[i].config = &config;
+		coders[i].left = &dongles[i];
+		coders[i].right = &dongles[(i + 1) % config.number_of_coder];
+		i++;
+	}
+	create_threads(config, coders);
+	i = 0;
+	while (i < config.number_of_coder)
+	{
+		pthread_mutex_destroy(&dongles[i].lock);
+		pthread_cond_destroy(&dongles[i].cond);
+		i++;
+	}
+	free(dongles);
+	free(coders);
 	return (0);
 }
