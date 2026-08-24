bd1b26e3911fcbb7448b087f3cd11f7ff60c21ce
Randriamanampisoa <fanilonomenjanahary062@gmail.com>
2026-08-24 23:16:00 +0300

    Modify init_coder_dongle function signature

--- codexion.h
+++ codexion.h
@@ -53,6 +53,6 @@ int		pars(t_data *config, int agrc, char *argv[]);
 int		main(int argc, char *argv[]);
 void	*create_threads(t_data config, t_coder *coders);
 void	*routine(void *arg);
-int     init_coder_dongle();
+int     init_coder_dongle(t_data config, t_coders *coders, t_dongle *dongle
 
 #endif
