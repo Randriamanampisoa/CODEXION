986231825b869bd666302ded6c9f8502ff83cb16
Randriamanampisoa <fanilonomenjanahary062@gmail.com>
2026-08-24 22:57:44 +0300

    Refactor dongle and coder initialization logic

--- main.c
+++ main.c
@@ -65,33 +65,8 @@ int	main(int argc, char *argv[])
 		printf("Error: Regarding the argument!");
 		return (1);
 	}
-	dongles = malloc(sizeof(t_dongle) * config.number_of_coder);
-	if (!dongles)
+	if (!init_coder_dongle(config, coders, dongles))
 		return (1);
-	i = 0;
-	while (i < config.number_of_coder)
-	{
-		pthread_mutex_init(&dongles[i].lock, NULL);
-		pthread_cond_init(&dongles[i].cond, NULL);
-		dongles[i].available = 1;
-		dongles[i].released_at = 0;
-		i++;
-	}
-	coders = malloc(sizeof(t_coder) * config.number_of_coder);
-	if (!coders)
-	{
-		free(dongles);
-		return (1);
-	}
-	i = 0;
-	while (i < config.number_of_coder)
-	{
-		coders[i].id = i + 1;
-		coders[i].config = &config;
-		coders[i].left = &dongles[i];
-		coders[i].right = &dongles[(i + 1) % config.number_of_coder];
-		i++;
-	}
 	create_threads(config, coders);
 	i = 0;
 	while (i < config.number_of_coder)
