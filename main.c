cdbeb72cd50e66b2c5e98a58bdbc9f8d972f50dd
fanilran <fanilonomenjanahary062@gmail.com>
2026-09-11 12:14:46 +0300

    update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fanilran <fanilran@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/16 04:30:25 by fanilran          #+#    #+#             */
-/*   Updated: 2026/09/08 17:54:43 by fanilran         ###   ########.fr       */
+/*   Updated: 2026/09/11 12:04:18 by fanilran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,18 +36,32 @@ int	check_burnout(t_coder *coders)
 	i = 0;
 	while (i < coders->config->number_of_coder)
 	{
-		pthread_mutex_lock(&coders[i].last_compile_start);
-		d = coders->last_compile_start + get_timestamp_ms(coders->config->start_time);
-		pthread_mutex_lock(&coders[i].last_compile_start);
-		n = get_current_ms();
-		if (n > d)
+		pthread_mutex_lock(&coders[i].activity_mutex);
+		d = coders[i].last_compile_start + coders[i].config->time_to_burnout;
+		pthread_mutex_unlock(&coders[i].activity_mutex);
+		n = get_timestamp_ms(coders[i].config->start_time);
+		if (n >= d)
 		{
-			printf("%ld %d is bunrout", n, coders[i].id);
+			printf("%ld %d is bunrout\n", n, coders[i].id);
 			return (1);
 		}
 		i++;
 	}
-	
+	return (0);
+}
+
+void	*monitor(void *arg)
+{
+	t_coder	*coder;
+
+	coder = (t_coder *)arg;
+	while (1)
+	{
+		if (check_burnout(coder))
+			break ;
+		usleep(1000);
+	}
+	return (NULL);	
 }
 
 void	*routine(void *arg)
@@ -79,6 +93,7 @@ void	*routine(void *arg)
 void	*create_threads(t_data *config, t_coder *coders)
 {
 	int			i;
+	pthread_t	monitor_t;
 
 	coders->thread = malloc(sizeof(pthread_t) * config->number_of_coder);
 	if (!coders->thread)
@@ -89,12 +104,14 @@ void	*create_threads(t_data *config, t_coder *coders)
 		pthread_create(&coders->thread[i], NULL, routine, &coders[i]);
 		i++;
 	}
+	pthread_create(&monitor_t, NULL, monitor, coders);
 	i = 0;
 	while (i < config->number_of_coder)
 	{
 		pthread_join(coders->thread[i], NULL);
 		i++;
 	}
+	pthread_join(monitor_t, NULL);
 	free(coders->thread);
 	return (NULL);
 }
